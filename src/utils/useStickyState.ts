9294ccfbd2295ed3895d14d8883aeb15bb93aa03
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-01-28 12:40:41 -0500

    Update useStickyState.ts
    fix error where app crashed after page refresh due to error with how the date object was stored in localStorage

--- src/utils/useStickyState.ts
+++ src/utils/useStickyState.ts
@@ -3,10 +3,23 @@ import React from 'react'
 function useStickyState<T> (key: string) {
   const [entries, setEntries] = React.useState<T>(() => {
     const stickyValue = window.localStorage.getItem(key)
-    return stickyValue !== null ? JSON.parse(stickyValue) : []
+    if (stickyValue === null) {
+      return []
+    }
+    return JSON.parse(stickyValue, (key: any, value: any) => {
+      if (typeof value === 'string' && value.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)) {
+        return new Date(value)
+      }
+      return value
+    })
   })
   React.useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(entries))
+    window.localStorage.setItem(key, JSON.stringify(entries, (key: any, value: any) => {
+      if (value instanceof Date) {
+        return value.toISOString()
+      }
+      return value
+    }))
   }, [key, entries])
   return { entries, setEntries }
 }
