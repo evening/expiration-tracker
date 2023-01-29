25ce57583983f050aadb76249a41b3950d36b5ca
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-01-29 14:15:41 -0500

    Update FoodItem.tsx

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { type Entry } from '../types/Entry'
 
 interface FoodItemProps {
@@ -6,10 +6,31 @@ interface FoodItemProps {
 }
 
 const FoodItem = ({ food }: FoodItemProps) => {
+  const [isNearExpiration, setIsNearExpiration] = React.useState<boolean>(false)
+  const [isExpired, setIsExpired] = React.useState<boolean>(false)
+
+  const today = new Date()
+  const DaysToWarning = 3
+
+  useEffect(() => {
+    if (food.expiration !== null && today > food.expiration) {
+      setIsNearExpiration(false)
+      setIsExpired(true)
+    } else if (food.expiration !== null && today.valueOf() > food.expiration.valueOf() - DaysToWarning * 24 * 60 * 60 * 1000) {
+      setIsNearExpiration(true)
+      setIsExpired(false)
+    } else {
+      setIsNearExpiration(false)
+      setIsExpired(false)
+    }
+  }, [food.expiration])
+
+  const conditionalClass = isExpired ? 'text-red-500' : isNearExpiration ? 'text-yellow-500' : 'text-green-700'
+
   return (
-    <p>
+    <p className={`${conditionalClass}`} >
       {food.foodName} | {(food.expiration != null) ? new Date(food?.expiration).toLocaleDateString() : ''}
-    </p>
+    </p >
   )
 }
 
