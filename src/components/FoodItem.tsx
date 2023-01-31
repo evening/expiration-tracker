fa2dfc5de1125a7e31a555ea024d64102ec10e84
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-01-31 10:35:46 -0500

    Update FoodItem.tsx
    replace two booleans states (isNearExpiration, isExpired) with a single string based on enums (status)

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { type Entry } from '../types/Entry'
+import { FoodStatus } from '../constants/FoodStatus'
+
 import dayjs from 'dayjs'
 
 interface FoodItemProps {
@@ -7,25 +9,21 @@ interface FoodItemProps {
 }
 
 const FoodItem = ({ food }: FoodItemProps) => {
-  const [isNearExpiration, setIsNearExpiration] = React.useState<boolean>(false)
-  const [isExpired, setIsExpired] = React.useState<boolean>(false)
+  const [status, setStatus] = React.useState<string>(FoodStatus.good)
   const [warning, setWarning] = React.useState<string>('')
 
   const today = dayjs().startOf('day')
   const DaysToWarning = 3
 
   useEffect(() => {
-      setIsNearExpiration(false)
-      setIsExpired(true)
     if (food.expiration !== null && dayjs(today).isAfter(dayjs(food.expiration), 'days')) {
+      setStatus(FoodStatus.expired)
       setWarning(' | Expired')
-      setIsNearExpiration(true)
-      setIsExpired(false)
     } else if (food.expiration !== null && dayjs(today).isAfter(dayjs(food.expiration).subtract(DaysToWarning, 'days'), 'days')) {
+      setStatus(FoodStatus.nearExpiration)
       setWarning(`| Expires in less than ${DaysToWarning} day[s]!`)
     } else {
-      setIsNearExpiration(false)
-      setIsExpired(false)
+      setStatus(FoodStatus.good)
       setWarning('')
     }
   }, [food.expiration])
