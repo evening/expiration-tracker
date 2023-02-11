a76081b087d81a89e6925816b9e7a739854e027f
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-02-11 17:38:50 -0500

    Update FoodList.tsx
    filter display for each column based on food location property

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -22,44 +22,94 @@ const FoodList = ({ entries, setEntries, searchTerm }: FoodListProps) => {
       </h2>
       <div className='grid grid-rows-3 lg:grid-cols-3 gap-4  '>
         <div>
-        <h3 className="underline decoration-gray-400 inline font-bold">
-          Fridge
-        </h3>
-        <div className="grid grid-cols-5">
-        <div className='col-span-1 font-semibold mx-auto'> Item </div>
-        <div className='col-span-1 font-semibold mx-auto'> Expiration </div>
-        <div className='col-span-1 font-semibold mx-auto'> Status </div>
-        <div className='col-span-2 font-semibold mx-auto'> Action </div>
-        {entries.filter(entry => { if (searchTerm != null) { return entry.foodName.includes(searchTerm) } return entry })
-          .map((food, index) =>
-            <Fragment key={index}>
-              <FoodItem food={food} />
-            <div className='col-span-2'>
-              <EditFoodButton
-                food={food}
-                index={index}
-                entries={entries}
-                setEntries={setEntries}
-              />
-              <DeleteFoodButton
-                food={food}
-                entries={entries}
-                setEntries={setEntries}
-              />
-            </div>
-              </Fragment>
-          )}
-        </div>
-        </div>
-        <div>
-        <h3 className="underline decoration-gray-400 inline font-bold">
-          Freezer
-        </h3>
+          <h3 className="underline decoration-gray-400 inline font-bold">
+            Fridge
+          </h3>
+          <div className="grid grid-cols-5">
+            <div className='col-span-1 font-semibold mx-auto'> Item </div>
+            <div className='col-span-1 font-semibold mx-auto'> Expiration </div>
+            <div className='col-span-1 font-semibold mx-auto'> Status </div>
+            <div className='col-span-2 font-semibold mx-auto'> Action </div>
+            {entries.filter(entry => entry.location === FoodLocation.fridge && entry.foodName.includes(searchTerm))
+              .map((food, index) =>
+                <Fragment key={index}>
+                  <FoodItem food={food} />
+                  <div className='col-span-2'>
+                    <EditFoodButton
+                      food={food}
+                      index={index}
+                      entries={entries}
+                      setEntries={setEntries}
+                    />
+                    <DeleteFoodButton
+                      food={food}
+                      entries={entries}
+                      setEntries={setEntries}
+                    />
+                  </div>
+                </Fragment>
+              )}
+          </div>
         </div>
         <div>
-        <h3 className="underline decoration-gray-400 inline font-bold">
-          Pantry
-        </h3>
+          <h3 className="underline decoration-gray-400 inline font-bold">
+            Freezer
+          </h3>
+          <div className="grid grid-cols-5">
+            <div className='col-span-1 font-semibold mx-auto'> Item </div>
+            <div className='col-span-1 font-semibold mx-auto'> Expiration </div>
+            <div className='col-span-1 font-semibold mx-auto'> Status </div>
+            <div className='col-span-2 font-semibold mx-auto'> Action </div>
+            {entries.filter(entry => entry.location === FoodLocation.freezer && entry.foodName.includes(searchTerm))
+              .map((food, index) =>
+                <Fragment key={index}>
+                  <FoodItem food={food} />
+                  <div className='col-span-2'>
+                    <EditFoodButton
+                      food={food}
+                      index={index}
+                      entries={entries}
+                      setEntries={setEntries}
+                    />
+                    <DeleteFoodButton
+                      food={food}
+                      entries={entries}
+                      setEntries={setEntries}
+                    />
+                  </div>
+                </Fragment>
+              )}
+          </div>
+        </div>
+        <div>
+          <h3 className="underline decoration-gray-400 inline font-bold">
+            Pantry
+          </h3>
+          <div className="grid grid-cols-5">
+            <div className='col-span-1 font-semibold mx-auto'> Item </div>
+            <div className='col-span-1 font-semibold mx-auto'> Expiration </div>
+            <div className='col-span-1 font-semibold mx-auto'> Status </div>
+            <div className='col-span-2 font-semibold mx-auto'> Action </div>
+            {entries.filter(entry => entry.location === FoodLocation.pantry && entry.foodName.includes(searchTerm))
+              .map((food, index) =>
+                <Fragment key={index}>
+                  <FoodItem food={food} />
+                  <div className='col-span-2'>
+                    <EditFoodButton
+                      food={food}
+                      index={index}
+                      entries={entries}
+                      setEntries={setEntries}
+                    />
+                    <DeleteFoodButton
+                      food={food}
+                      entries={entries}
+                      setEntries={setEntries}
+                    />
+                  </div>
+                </Fragment>
+              )}
+          </div>
         </div>
       </div>
     </div>
