c2482227546f987a8faf5ce68af167177184fdf9
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-03-04 15:38:11 -0500

    add uuids to entries and storage locations

--- src/types/Entry.ts
+++ src/types/Entry.ts
@@ -2,14 +2,16 @@ interface Entry {
   foodName: string
   expiration: Date | null
   location: string
+  id: string
 }
 
 interface EntryFromStorage {
   foodName: string
   expiration: string | null
   location: string
+  id: string
 }
 
 type Entries = Entry[]
 
-export type { Entry, Entries, EntryFromStorage }
+export type { Entry, EntryFromStorage, Entries }
