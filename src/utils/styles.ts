85853cf77a43b26478c1974ac0d9fbee76703a4b
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-03-05 14:48:56 -0500

    Create styles.ts
    utility functions for styling while dragging, background colors, etc.

---
+++ src/utils/styles.ts
@@ -0,0 +1,12 @@
+export const getBackgroundColor = (
+  isDraggingOver: boolean,
+  isDraggingFrom: boolean
+): string => {
+  if (isDraggingOver) {
+    return 'bg-red-100'
+  }
+  if (isDraggingFrom) {
+    return 'bg-yellow-100'
+  }
+  return 'bg-white'
+}
