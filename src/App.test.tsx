51674d493140213d273416f0a67d2e14a7047ee7
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-04-15 14:36:12 -0400

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('home page loads', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Food List/i)
+  const linkElement = screen.getByText(/Food Tracker/i)
   expect(linkElement).toBeInTheDocument()
 })
