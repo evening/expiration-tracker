f2cc78b906498582bb1e765d95a21df57197fe48
Joseph Belmonte <49847370+joseph-belmonte@users.noreply.github.com>
2023-05-16 20:17:57 -0400

    Update SearchBar.tsx
    button that expands on click for searching; fixed on viewport similar to add button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import SearchIcon from '@mui/icons-material/Search'
+
 interface SearchBarProps {
   searchTerm: string
   setSearchTerm: (entries: any) => void
@@ -7,19 +9,32 @@ interface SearchBarProps {
 
 const SearchBar = ({ searchTerm, setSearchTerm }: SearchBarProps) => {
   return (
-    <div className='py-4'>
-      {/* TODO: Add magnifying glass svg */}
-      <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"> Search </label>
-          <input
-          type="search"
-          id="search"
-          className='py-1 px-2 pl-10 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500'
-          placeholder="Search"
+    <div id='content' className='bg-transparent'>
+      <label htmlFor='search' className='sr-only'> Search </label>
+      <div
+        dir='ltr'
+        id='search-bar'
+        className='group flex border-2 border-primary-300 rounded-[50px] h-[40px] w-11 transition-all duration-500 focus-within:w-[33%] focus-within:opacity-90 overflow-hidden ml-auto fixed z-10 bottom-20 right-5 overflow-y-auto '
+      >
+        <input
+          type={'search'}
+          placeholder='Apple'
+          id='search-bar__input'
+          aria-label='Search'
+          className='flex-grow px-2 !border-0 focus:outline-0 !rounded-l-[50px] absolute top-0 bottom-0 left-0 opacity-0 group-focus-within:opacity-100 transition-all duration-500 cursor-pointer group-focus-within:cursor-default group-focus-within:w-[100%] text-primary-300'
           onChange={(e) => { setSearchTerm(e.target.value) }}
           value={searchTerm}
           required
-          />
+        />
+        <button
+          id='search-bar__submit'
+          aria-label='Submit search'
+          className='cursor-pointer border-0 rounded-full bg-primary-300 px-2 text-white h-[34px] my-auto ml-auto  group-focus-within:outline-none transition-all duration-500 group-focus-within:focus:outline-0  !group-focus-within:hover:shadow-md z-10 active:bg-secondary-300 focus:ring-4  focus:ring-secondary-300'
+        >
+          <SearchIcon />
+        </button>
       </div>
+    </div>
   )
 }
 
